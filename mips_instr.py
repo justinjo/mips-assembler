2505c62df126baf7668f25ef99b6ec2369d10245
Justin Jo <justinjo@users.noreply.github.com>
2017-12-07 13:52:03 -0500

    Added register numbers

--- mips_instr.py
+++ mips_instr.py
@@ -2,58 +2,105 @@ class MipsIsa:
 	'''
 	TODO: add documentation
 	Core instruction set and pseudoinstructions added
+	If any of the exported functions fail, they return None
 	'''
 
 	OP  = 'opcode'
 	FUN = 'funct'
-	FRM = 'format'
-	FMT = 'fmt'
-	FT  = 'ft'
+	FMT = 'format'
 
 	ISA =
 	{
 		# core instruction set
-		'add'   : {OP: 0,  FUN: 32,   FRM: 'R'},
-		'addi'  : {OP: 8,  FUN: None, FRM: 'I'},
-		'addiu' : {OP: 9,  FUN: None, FRM: 'I'},
-		'addu'  : {OP: 0,  FUN: 33,   FRM: 'R'},
-		'and'   : {OP: 0,  FUN: 36,   FRM: 'R'},
-		'andi'  : {OP: 12, FUN: 36,   FRM: 'I'},
-		'beq'   : {OP: 4,  FUN: None, FRM: 'I'},
-		'bne'   : {OP: 5,  FUN: None, FRM: 'I'},
-		'j'     : {OP: 2,  FUN: None, FRM: 'J'},
-		'jal'   : {OP: 3,  FUN: None, FRM: 'J'},
-		'jr'    : {OP: 0,  FUN: 8,    FRM: 'R'},
-		'lbu'   : {OP: 36, FUN: None, FRM: 'I'},
-		'lhu'   : {OP: 37, FUN: None, FRM: 'I'},
-		'll'    : {OP: 48, FUN: None, FRM: 'I'},
-		'lui'   : {OP: 15, FUN: None, FRM: 'I'},
-		'lw'    : {OP: 35, FUN: None, FRM: 'I'},
-		'nor'   : {OP: 0,  FUN: 39,   FRM: 'R'},
-		'or'    : {OP: 0,  FUN: 37,   FRM: 'R'},
-		'ori'   : {OP: 13, FUN: None, FRM: 'I'},
-		'slt'   : {OP: 0,  FUN: 42,   FRM: 'R'},
-		'slti'  : {OP: 10, FUN: None, FRM: 'I'},
-		'sltiu' : {OP: 11, FUN: None, FRM: 'I'},
-		'sltu'  : {OP: 0,  FUN: 43,   FRM: 'R'},
-		'sll'   : {OP: 0,  FUN: 0,    FRM: 'R'},
-		'srl'   : {OP: 0,  FUN: 2,    FRM: 'R'},
-		'sb'    : {OP: 40, FUN: None, FRM: 'I'},
-		'sc'    : {OP: 56, FUN: None, FRM: 'I'},
-		'sh'    : {OP: 41, FUN: None, FRM: 'I'},
-		'sw'    : {OP: 43, FUN: None, FRM: 'I'},
-		'sub'   : {OP: 0,  FUN: 34,   FRM: 'R'},
-		'subu'  : {OP: 0,  FUN: 35,   FRM: 'R'},
+		'add'   : {OP: 0,  FUN: 32,   FMT: 'R'},
+		'addi'  : {OP: 8,  FUN: None, FMT: 'I'},
+		'addiu' : {OP: 9,  FUN: None, FMT: 'I'},
+		'addu'  : {OP: 0,  FUN: 33,   FMT: 'R'},
+		'and'   : {OP: 0,  FUN: 36,   FMT: 'R'},
+		'andi'  : {OP: 12, FUN: 36,   FMT: 'I'},
+		'beq'   : {OP: 4,  FUN: None, FMT: 'I'},
+		'bne'   : {OP: 5,  FUN: None, FMT: 'I'},
+		'j'     : {OP: 2,  FUN: None, FMT: 'J'},
+		'jal'   : {OP: 3,  FUN: None, FMT: 'J'},
+		'jr'    : {OP: 0,  FUN: 8,    FMT: 'R'},
+		'lbu'   : {OP: 36, FUN: None, FMT: 'I'},
+		'lhu'   : {OP: 37, FUN: None, FMT: 'I'},
+		'll'    : {OP: 48, FUN: None, FMT: 'I'},
+		'lui'   : {OP: 15, FUN: None, FMT: 'I'},
+		'lw'    : {OP: 35, FUN: None, FMT: 'I'},
+		'nor'   : {OP: 0,  FUN: 39,   FMT: 'R'},
+		'or'    : {OP: 0,  FUN: 37,   FMT: 'R'},
+		'ori'   : {OP: 13, FUN: None, FMT: 'I'},
+		'slt'   : {OP: 0,  FUN: 42,   FMT: 'R'},
+		'slti'  : {OP: 10, FUN: None, FMT: 'I'},
+		'sltiu' : {OP: 11, FUN: None, FMT: 'I'},
+		'sltu'  : {OP: 0,  FUN: 43,   FMT: 'R'},
+		'sll'   : {OP: 0,  FUN: 0,    FMT: 'R'},
+		'srl'   : {OP: 0,  FUN: 2,    FMT: 'R'},
+		'sb'    : {OP: 40, FUN: None, FMT: 'I'},
+		'sc'    : {OP: 56, FUN: None, FMT: 'I'},
+		'sh'    : {OP: 41, FUN: None, FMT: 'I'},
+		'sw'    : {OP: 43, FUN: None, FMT: 'I'},
+		'sub'   : {OP: 0,  FUN: 34,   FMT: 'R'},
+		'subu'  : {OP: 0,  FUN: 35,   FMT: 'R'},
+	}
+
+	REG = 
+	{
+		'$zero' : 0,
+		'$at'   : 1,
+		'$v0'   : 2,
+		'$v1'   : 3,
+		'$a0' : 4,
+		'$a1' : 5,
+		'$a2' : 6,
+		'$a3' : 7,
+		'$t0' : 8,
+		'$t1' : 9,
+		'$t2' : 10,
+		'$t3' : 11,
+		'$t4' : 12,
+		'$t5' : 13,
+		'$t6' : 14,
+		'$t7' : 15,
+		'$s0' : 16,
+		'$s1' : 17,
+		'$s2' : 18,
+		'$s3' : 19,
+		'$s4' : 20,
+		'$s5' : 21,
+		'$s6' : 22,
+		'$s7' : 23,
+		'$t8' : 24,
+		'$t9' : 25,
+		'$k0' : 26,
+		'$k1' : 27,
+		'$gp' : 28,
+		'$sp' : 29,
+		'$fp' : 30,
+		'$ra' : 31
 	}
 
 	def __init__(self):
 		pass
 
 	def getOpcode(self, instr):
-		return ISA[instr][OP]
+		if instr not in self.ISA:
+			return
+		return self.ISA[instr][OP]
 
 	def getFunction(self, instr):
-		return ISA[instr][FUN]
+		if instr not in self.ISA:
+			return
+		return self.ISA[instr][FUN]
 
 	def getFormat(self, instr):
-		return ISA[instr][FMT]
+		if instr not in self.ISA:
+			return
+		return self.ISA[instr][FMT]
+
+	def getRegNum(self, reg):
+		if reg not in self.REG:
+			return
+		return self.REG[reg]
+
